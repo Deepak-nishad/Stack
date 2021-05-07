870bc3160a43d6aeaa91a3675359586f458a7f99
Deepak <62493802+Deepak-nishad@users.noreply.github.com>
2021-05-07 10:30:45 +0530

    Add files via upload

---
+++ stack_using array.cpp
@@ -0,0 +1,101 @@
+#include<climits>
+#include<iostream>
+using namespace std;
+class stack{
+	int *data;
+	int next_index;
+	int capacity;
+	public:
+		stack(int totalsize)
+		{
+			data=new int[totalsize];
+			next_index=-1;
+			capacity=totalsize;
+		}
+		
+		// lets create function how many element present in array
+		int size()
+		{
+			return next_index;
+		}
+		
+		//lets create finction our stack is empty or not 
+		
+		int is_empty()
+		{
+			if(next_index==-1)
+			return true ;
+			else
+			return false;
+			
+			//return next_index==-1;
+		}
+		
+		// push value in stack 
+		
+		void push(int value)
+		{
+			if(next_index==capacity-1)
+			{
+				cout<<"Stack is full "<<endl;
+				return;
+			}
+			
+			next_index++;
+			data[next_index]=value;
+			
+		}
+		// delete the element from stack
+		int pop()
+		{
+			int pop_value;
+			if(next_index==-1)
+			{
+				cout<<"Nothing is in stack ";
+				return INT_MIN;
+			}
+			else
+			{
+				 pop_value=data[next_index];
+				next_index--;
+			}
+			return pop_value; 
+		}
+		
+		// what element is on the top
+		int top()
+		{
+			return data[next_index];
+		}
+};
+
+int main()
+{
+	stack s(5);
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(40);
+	s.push(50);
+	 
+	 int pop_value=s.pop();
+	 cout<<pop_value<<endl;
+	  pop_value=s.pop();
+	 cout<<pop_value<<endl;
+	  pop_value=s.pop();
+	 cout<<pop_value<<endl;
+	  pop_value=s.pop();
+	 cout<<pop_value<<endl;
+	  pop_value=s.pop();
+	 cout<<pop_value<<endl;
+	  pop_value=s.pop();
+	 cout<<pop_value<<endl;
+	 
+	 cout<<s.size()<<endl;
+	 s.push(50);
+	 s.push(100);
+	 cout<<s.top()<<endl;
+	 
+	
+}
